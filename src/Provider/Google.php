3c2f34ea188bbfc3261f4bdc2c8dbd7342456ff3
Jagepard <dankorot@gmail.com>
2020-01-16 15:01:18 +0300

    Add Provider

---
+++ src/Provider/Google.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OAuthClient\Provider;
+
+class Google extends AbstractProvider
+{
+    /**
+     * @param  array  $config
+     */
+    public function __construct(array $config)
+    {
+        parent::__construct($config);
+
+        $this->urls = [
+            'auth'         => 'https://accounts.google.com/o/oauth2/auth',
+            'access_token' => 'https://accounts.google.com/o/oauth2/token',
+            'remote_api'   => 'https://www.googleapis.com/oauth2/v1/userinfo',
+        ];
+    }
+
+    /**
+     * @param  string|null  $code
+     */
+    public function authenticate(string $code = null): void
+    {
+        if (isset($code)) {
+            $params = [
+                'grant_type' => 'authorization_code',
+                'code'       => $code,
+            ];
+
+            $token = $this->request($params);
+
+            if (array_key_exists('access_token', $token)) {
+                $params = [
+                    'format'      => 'json',
+                    'oauth_token' => $token['access_token'],
+                ];
+
+                $this->urls['remote_api'] = $this->urls['remote_api'].'?'.http_build_query($params);
+                $this->user               = $this->request();
+            }
+        }
+    }
+}
