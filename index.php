fdc8d89998678e0c88868b66522218b72f657c79
Jagepard <dankorot@gmail.com>
2020-01-27 11:23:35 +0300

    update

--- index.php
+++ index.php
@@ -2,45 +2,54 @@
 
 require_once './vendor/autoload.php';
 
-$oauthClient = new OAuthClient\OAuthClient(
-    new OAuthClient\Provider\Yandex(
-        [
-            'client_id'     => 'your client_id',
-            'client_secret' => 'your client_secret',
-            'redirect_uri'  => 'yoor redirect_uri',
-        ]
-    ),
-    new OAuthClient\Provider\Github(
-        [
-            'client_id'     => 'your client_id',
-            'client_secret' => 'your client_secret',
-            'redirect_uri'  => 'yoor redirect_uri',
-        ]
-    ),
-    new OAuthClient\Provider\Google(
-        [
-            'client_id'     => 'your client_id',
-            'client_secret' => 'your client_secret',
-            'redirect_uri'  => 'yoor redirect_uri',
-        ]
-    ),
-    new OAuthClient\Provider\Facebook(
-        [
-            'client_id'     => 'your client_id',
-            'client_secret' => 'your client_secret',
-            'redirect_uri'  => 'yoor redirect_uri',
-        ]
-    )
-);
+use OAuthClient\OAuthClient;
+use OAuthClient\Provider\Github;
+use OAuthClient\Provider\Google;
+use OAuthClient\Provider\ProviderInterface;
+use OAuthClient\Provider\Yandex;
+
+$providers = [
+    new Yandex([
+        'client_id'     => 'your_client_id',
+        'client_secret' => 'your_client_secret',
+        'redirect_uri'  => 'your_redirect_uri'
+    ]),    
+    new Github([
+        'client_id'     => 'your_client_id',
+        'client_secret' => 'your_client_secret',
+        'redirect_uri'  => 'your_redirect_uri'
+    ]),    
+    new Google([
+        'client_id'     => 'your_client_id',
+        'client_secret' => 'your_client_secret',
+        'redirect_uri'  => 'your_redirect_uri'
+    ])
+];
+
+$oauthClient = new OAuthClient($providers);
+
+(new Whoops\Run)->appendHandler(new Whoops\Handler\PrettyPageHandler)->register();
 
 $extraOptions = [
     'google'   => ['scope' => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'],
-    'facebook' => ['scope' => 'email,user_birthday']
+    'facebook' => ['scope' => 'email']
 ];
 
-if (!isset($_GET['code'])) {
-    printf("<p><a href=\"%s\">Authentication</a>", $oauthClient->provider(\OAuthClient\Provider\Facebook::class)->url());
-} else {
-    $oauthClient->provider(\OAuthClient\Provider\Facebook::class)->authenticate($_GET['code']);
-    var_dump($oauthClient->provider(\OAuthClient\Provider\Facebook::class)->user());
+function handle(OAuthClient $oauthClient, $getRequest = null, ProviderInterface $provider, $extraOptions = []) {
+    if (!isset($getRequest['code'])) {
+        printf("<p><a href=\"%s\">Authentication: {$provider->getName()}</a>", $oauthClient->provider($provider->getName())->url($extraOptions));
+    } else {
+        if (isset($getRequest['provider']) && $getRequest['provider'] === $provider->getName()) {
+            $oauthClient->provider($provider->getName())->authenticate($getRequest['code']);
+            var_dump($oauthClient->provider($provider->getName())->user());
+        }
+    }
+}
+
+foreach ($providers as $provider) {
+    if ($provider->getName() === 'google') {
+        handle($oauthClient, $_GET, $provider, $extraOptions['google']);
+    } else {
+        handle($oauthClient, $_GET, $provider);
+    }
 }
